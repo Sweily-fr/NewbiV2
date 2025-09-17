79962acf7d06636d6ff6261d0ca1e0a78924e5da
joaquimgameiro <joaquim.progmr@gmail.com>
2025-09-17 22:28:24 +0200

    changement better auth url

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -9,7 +9,7 @@ import {
 import { stripeClient } from "@better-auth/stripe/client";
 
 export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000",
+  baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL || "http://localhost:3000",
   plugins: [
     adminClient(),
     phoneNumberClient(),
