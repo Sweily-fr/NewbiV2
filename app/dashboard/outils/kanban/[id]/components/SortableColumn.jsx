7ce621f29717b8a9c0584c0b40911aacd1ab5d74
Dylan <dylan.lobjois@sweily.fr>
2025-10-08 00:30:47 +0200

    drag and drop

---
+++ app/dashboard/outils/kanban/[id]/components/SortableColumn.jsx
@@ -0,0 +1,50 @@
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+import React from "react";
+
+/**
+ * Composant pour rendre une colonne draggable avec dnd-kit
+ * Le header de la colonne est draggable pour réorganiser les colonnes
+ */
+export function SortableColumn({ column, children }) {
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({
+    id: column.id,
+    data: {
+      type: "column",
+      column,
+    },
+    transition: {
+      duration: 200, // Durée de la transition en ms
+      easing: "cubic-bezier(0.25, 1, 0.5, 1)", // Courbe d'accélération fluide
+    },
+  });
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition: transition || undefined,
+    willChange: "transform",
+  };
+
+  // Cloner l'enfant et ajouter les props de drag au header
+  const childWithDragProps = React.cloneElement(children, {
+    dragHandleProps: { ...attributes, ...listeners },
+    isDragging,
+  });
+
+  return (
+    <div
+      ref={setNodeRef}
+      style={style}
+      className="flex-shrink-0"
+    >
+      {childWithDragProps}
+    </div>
+  );
+}
