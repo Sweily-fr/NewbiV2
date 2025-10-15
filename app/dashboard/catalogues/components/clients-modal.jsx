cfd563234ea438f17755c94a2abf1f20d34a8536
joaquimgameiro <joaquim.progmr@gmail.com>
2025-10-15 19:05:51 +0200

    reponsive modal client et catalogue

--- app/dashboard/catalogues/components/clients-modal.jsx
+++ app/dashboard/catalogues/components/clients-modal.jsx
@@ -145,7 +145,6 @@ export default function ClientsModal({ client, onSave, open, onOpenChange }) {
           className="flex flex-col flex-1 min-h-0"
         >
           <div className="flex-1 overflow-y-auto px-6 py-4 space-y-4">
-          <div className="space-y-4">
             {/* Type de client */}
             <div className="space-y-2">
               <Label>Type de client *</Label>
@@ -323,7 +322,6 @@ export default function ClientsModal({ client, onSave, open, onOpenChange }) {
                 </div>
               </div>
             )}
-          </div>
           </div>
 
           {/* Footer dans le flux flex - s'adapte automatiquement à Safari */}
--- app/dashboard/catalogues/components/product-modal.jsx
+++ app/dashboard/catalogues/components/product-modal.jsx
@@ -219,7 +219,6 @@ export default function ProductModal({ product, onSave, open, onOpenChange }) {
           className="flex flex-col flex-1 min-h-0"
         >
           <div className="flex-1 overflow-y-auto px-6 py-4 space-y-4">
-          <div className="space-y-4">
             {/* Nom du produit */}
             <div className="space-y-2">
               <Label className="font-normal">Nom du produit *</Label>
@@ -387,7 +386,6 @@ export default function ProductModal({ product, onSave, open, onOpenChange }) {
                 </div>
               </div>
             </div>
-          </div>
           </div>
 
           {/* Footer dans le flux flex - s'adapte automatiquement à Safari */}
