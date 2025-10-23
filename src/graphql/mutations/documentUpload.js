437042e7a7da0fe328eab4954281cde64980e4fd
Dylan <dylan.lobjois@sweily.fr>
2025-10-23 23:21:40 +0200

    image cloudflare temp

--- src/graphql/mutations/documentUpload.js
+++ src/graphql/mutations/documentUpload.js
@@ -18,6 +18,17 @@ export const UPLOAD_DOCUMENT = gql`
   }
 `;
 
+export const PROMOTE_TEMPORARY_FILE = gql`
+  mutation PromoteTemporaryFile($tempKey: String!) {
+    promoteTemporaryFile(tempKey: $tempKey) {
+      success
+      key
+      url
+      message
+    }
+  }
+`;
+
 export const DELETE_DOCUMENT = gql`
   mutation DeleteDocument($key: String!) {
     deleteDocument(key: $key) {
