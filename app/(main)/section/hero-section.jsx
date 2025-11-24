93f34a0d5a2c4cd2636ecc82ce22c00a4e59fcf9
joaquimgameiro <joaquim.progmr@gmail.com>
2025-11-24 10:58:55 +0100

    navbar position black friday

--- app/(main)/section/hero-section.jsx
+++ app/(main)/section/hero-section.jsx
@@ -10,7 +10,7 @@ export function HeroSection() {
   return (
     <>
       <main className="overflow-hidden">
-        <section className="h-[90vh] md:h-[98vh] flex flex-col justify-between bg-gradient-to-t from-[#fce8e6] via-[#f8f9fc] to-[#f0f4ff] rounded-[15px] md:rounded-[18px] lg:rounded-[18px] shadow-xs mx-2 mt-2">
+        <section className="h-[90vh] md:h-[98vh] flex flex-col justify-between bg-gradient-to-t from-[#fce8e6] via-[#f8f9fc] to-[#f0f4ff] rounded-[15px] md:rounded-[18px] lg:rounded-[18px] shadow-xs mx-2 mt-16 md:mt-2">
           <BlackFridayBanner />
           <div className="flex-1 flex items-center justify-center">
             <HeroAnimation />
