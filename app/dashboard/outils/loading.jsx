c99e2bb0796cf53c2168213c3d901c21eea71130
Sweily-fr <marketing@sweily.fr>
2026-01-21 22:19:45 +0100

    delete skeleton main

--- app/dashboard/outils/loading.jsx
+++ app/dashboard/outils/loading.jsx
@@ -1,29 +1,33 @@
-import { Skeleton } from "@/src/components/ui/skeleton";
+// import { Skeleton } from "@/src/components/ui/skeleton";
 
+// Skeleton désactivé - chaque page gère son propre loading state
 export default function loader() {
-  return (
-    <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6 p-6">
-      <Skeleton className="h-[40px] w-[100px] rounded-xl" />
-      <div className="flex items-center justify-between">
-        <Skeleton className="h-[40px] w-[300px] rounded-xl" />
-        <Skeleton className="h-[40px] w-[300px] rounded-xl" />
-      </div>
-      <div className="grid grid-cols-3 gap-6 w-full">
-        {/* Première ligne */}
-        <Skeleton className="h-[200px] w-full rounded-xl" />
-        <Skeleton className="h-[200px] w-full rounded-xl" />
-        <Skeleton className="h-[200px] w-full rounded-xl" />
+  return null;
 
-        {/* Deuxième ligne */}
-        <Skeleton className="h-[200px] w-full rounded-xl" />
-        <Skeleton className="h-[200px] w-full rounded-xl" />
-        <Skeleton className="h-[200px] w-full rounded-xl" />
+  // Ancien skeleton commenté :
+  // return (
+  //   <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6 p-6">
+  //     <Skeleton className="h-[40px] w-[100px] rounded-xl" />
+  //     <div className="flex items-center justify-between">
+  //       <Skeleton className="h-[40px] w-[300px] rounded-xl" />
+  //       <Skeleton className="h-[40px] w-[300px] rounded-xl" />
+  //     </div>
+  //     <div className="grid grid-cols-3 gap-6 w-full">
+  //       {/* Première ligne */}
+  //       <Skeleton className="h-[200px] w-full rounded-xl" />
+  //       <Skeleton className="h-[200px] w-full rounded-xl" />
+  //       <Skeleton className="h-[200px] w-full rounded-xl" />
 
-        {/* Troisième ligne */}
-        <Skeleton className="h-[200px] w-full rounded-xl" />
-        <Skeleton className="h-[200px] w-full rounded-xl" />
-        <Skeleton className="h-[200px] w-full rounded-xl" />
-      </div>
-    </div>
-  );
+  //       {/* Deuxième ligne */}
+  //       <Skeleton className="h-[200px] w-full rounded-xl" />
+  //       <Skeleton className="h-[200px] w-full rounded-xl" />
+  //       <Skeleton className="h-[200px] w-full rounded-xl" />
+
+  //       {/* Troisième ligne */}
+  //       <Skeleton className="h-[200px] w-full rounded-xl" />
+  //       <Skeleton className="h-[200px] w-full rounded-xl" />
+  //       <Skeleton className="h-[200px] w-full rounded-xl" />
+  //     </div>
+  //   </div>
+  // );
 }
